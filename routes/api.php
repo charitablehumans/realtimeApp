5db676c04805b9f92a8a038c5bf6d347f64c272e
juarisco <juarisco@outlook.com>
2019-01-04 11:26:25 -0500

    Show replies

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@
 // Route::get('/question', 'QuestionController@index');
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
+Route::apiResource('/question/{question}/reply', 'ReplyController');
