f50bba5a07451e7f90e93e3cf5db7600e35a6c10
juarisco <juarisco@outlook.com>
2019-01-06 15:03:23 -0500

    Home components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Single Page Forum</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
         <v-app>
-            
+            <app-home></app-home>
         </v-app>
     </div>
 
