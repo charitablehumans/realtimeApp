ba74d8e5280b66e96eae2bad1f723a40a4a7d85c
juarisco <juarisco@outlook.com>
2019-01-08 11:27:45 -0500

    Signup component

--- resources/assets/js/Router/router.js
+++ resources/assets/js/Router/router.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../components/login/Login";
+import signup from "../components/login/signup";
 
 Vue.use(VueRouter);
-const routes = [{ path: "/login", component: Login }];
+const routes = [
+    { path: "/login", component: Login },
+    { path: "/signup", component: signup }
+];
 
 const router = new VueRouter({
     routes, // short for `routes: routes`
--- resources/assets/js/components/login/Login.vue
+++ resources/assets/js/components/login/Login.vue
@@ -6,6 +6,9 @@
 
       <v-btn color="green" type="submit">Login</v-btn>
       <!-- <v-btn @click="clear">clear</v-btn> -->
+      <router-link to="/signup">
+        <v-btn color="blue">Sign Up</v-btn>
+      </router-link>
     </v-form>
   </v-container>
 </template>
