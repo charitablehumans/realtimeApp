7e54780cf6182d5e28ef9f821974b859e884e4f2
juarisco <juarisco@outlook.com>
2019-01-09 15:29:19 -0500

    Show single Question

--- resources/assets/js/components/forum/read.vue
+++ resources/assets/js/components/forum/read.vue
@@ -1,7 +1,12 @@
-<template></template>
+<template>
+  <show-question :data="question"></show-question>
+</template>
 
 <script>
+import ShowQuestion from "./ShowQuestion";
+
 export default {
+  components: { ShowQuestion },
   data() {
     return {
       question: {}
