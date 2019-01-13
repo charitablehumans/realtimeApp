d450e41790621ea4731e5f25c770c5b3afa43576
juarisco <juarisco@outlook.com>
2019-01-12 20:18:26 -0500

    Send notifications

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -7,6 +7,7 @@ use App\Model\Question;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Resources\ReplyResource;
+use App\Notifications\NewReplyNotification;
 
 class ReplyController extends Controller
 {
@@ -41,6 +42,9 @@ class ReplyController extends Controller
     public function store(Question $question, Request $request)
     {
         $reply = $question->replies()->create($request->all());
+
+        $user = $question->user;
+        $user->notify(new NewReplyNotification($reply));
 
         return response(['reply' => new ReplyResource($reply)], Response::HTTP_CREATED);
 
--- app/Notifications/NewReplyNotification.php
+++ app/Notifications/NewReplyNotification.php
@@ -6,19 +6,22 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use App\Model\Reply;
 
 class NewReplyNotification extends Notification
 {
     use Queueable;
+
+    public $reply;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Reply $reply)
     {
-        //
+        $this->reply = $reply;
     }
 
     /**
@@ -38,13 +41,13 @@ class NewReplyNotification extends Notification
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
+    // public function toMail($notifiable)
+    // {
+    //     return (new MailMessage)
+    //         ->line('The introduction to the notification.')
+    //         ->action('Notification Action', url('/'))
+    //         ->line('Thank you for using our application!');
+    // }
 
     /**
      * Get the array representation of the notification.
@@ -55,7 +58,9 @@ class NewReplyNotification extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'replyBy' => $this->reply->user->name,
+            'question' => $this->reply->question->title,
+            'path' => $this->reply->question->path
         ];
     }
 }
